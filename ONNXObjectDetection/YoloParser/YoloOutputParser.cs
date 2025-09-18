354698cd1129303c547eaf8674fdd65af601efe5
Szilágyi Csaba <csabi32105@gmail.com>
2025-09-18 18:34:54 +0200

    Helper functions and output parsing

--- ONNXObjectDetection/YoloParser/YoloOutputParser.cs
+++ ONNXObjectDetection/YoloParser/YoloOutputParser.cs
@@ -53,4 +53,169 @@ public class YoloOutputParser
         Color.SandyBrown,
         Color.DarkTurquoise
     };
+
+    /// <summary>
+    /// Applies the sigmoid function that outputs a number between 0 and 1
+    /// </summary>    
+    private float Sigmoid(float value)
+    {
+        var k = (float)Math.Exp(value);
+        return k / (1.0f + k);
+    }
+
+    /// <summary>
+    /// Normalizes an input vector into a probability distribution
+    /// </summary>    
+    private float[] Softmax(float[] values)
+    {
+        var maxVal = values.Max();
+        var exp = values.Select(v => Math.Exp(v - maxVal));
+        var sumExp = exp.Sum();
+
+        return exp.Select(v => (float)(v / sumExp)).ToArray();
+    }
+
+    /// <summary>
+    /// Maps elements in the one-dimensional model output to the corresponding position in a 125 x 13 x 13 tensor
+    /// </summary>    
+    private int GetOffset(int x, int y, int channel)
+    {
+        // YOLO outputs a tensor that has a shape of 125x13x13, which 
+        // WinML flattens into a 1D array.  To access a specific channel 
+        // for a given (x,y) cell position, we need to calculate an offset
+        // into the array
+        return (channel * channelStride) + (y * COL_COUNT) + x;
+    }
+
+    /// <summary>
+    /// Extracts the bounding box dimensions using the GetOffset method from the model output
+    /// </summary>
+    private BoundingBoxDimensions ExtractBoundingBoxDimensions(float[] modelOutput, int x, int y, int channel)
+    {
+        return new BoundingBoxDimensions
+        {
+            X = modelOutput[GetOffset(x, y, channel)],
+            Y = modelOutput[GetOffset(x, y, channel + 1)],
+            Width = modelOutput[GetOffset(x, y, channel + 2)],
+            Height = modelOutput[GetOffset(x, y, channel + 3)]
+        };
+    }
+
+    /// <summary>
+    /// Extracts the confidence value that states how sure the model is that it has detected an object and uses the Sigmoid function to turn it into a percentage.
+    /// </summary>
+    private float GetConfidence(float[] modelOutput, int x, int y, int channel)
+    {
+        return Sigmoid(modelOutput[GetOffset(x, y, channel + 4)]);
+    }
+
+    /// <summary>
+    /// Uses the bounding box dimensions and maps them onto its respective cell within the image.
+    /// </summary>
+    private CellDimensions MapBoundingBoxToCell(int x, int y, int box, BoundingBoxDimensions boxDimensions)
+    {
+        return new CellDimensions
+        {
+            X = ((float)x + Sigmoid(boxDimensions.X)) * CELL_WIDTH,
+            Y = ((float)y + Sigmoid(boxDimensions.Y)) * CELL_HEIGHT,
+            Width = (float)Math.Exp(boxDimensions.Width) * CELL_WIDTH * anchors[box * 2],
+            Height = (float)Math.Exp(boxDimensions.Height) * CELL_HEIGHT * anchors[box * 2 + 1],
+        };
+    }
+
+    /// <summary>
+    /// Extracts the class predictions for the bounding box from the model output using the GetOffset method and turns them into a probability distribution using the Softmax method.
+    /// </summary>    
+    private float[] ExtractClasses(float[] modelOutput, int x, int y, int channel)
+    {
+        float[] predictedClasses = new float[CLASS_COUNT];
+        int predictedClassOffset = channel + BOX_INFO_FEATURE_COUNT;
+        for (int predictedClass = 0; predictedClass < CLASS_COUNT; predictedClass++)
+        {
+            predictedClasses[predictedClass] = modelOutput[GetOffset(x, y, predictedClass + predictedClassOffset)];
+        }
+        return Softmax(predictedClasses);
+    }
+
+    /// <summary>
+    /// selects the class from the list of predicted classes with the highest probability.
+    /// </summary>    
+    private ValueTuple<int, float> GetTopResult(float[] predictedClasses)
+    {
+        return predictedClasses
+            .Select((predictedClass, index) => (Index: index, Value: predictedClass))
+            .OrderByDescending(result => result.Value)
+            .First();
+    }
+
+    /// <summary>
+    /// Filters overlapping bounding boxes with lower probabilities.
+    /// </summary>    
+    private float IntersectionOverUnion(RectangleF boundingBoxA, RectangleF boundingBoxB)
+    {
+        var areaA = boundingBoxA.Width * boundingBoxA.Height;
+
+        if (areaA <= 0)
+            return 0;
+
+        var areaB = boundingBoxB.Width * boundingBoxB.Height;
+
+        if (areaB <= 0)
+            return 0;
+
+        var minX = Math.Max(boundingBoxA.Left, boundingBoxB.Left);
+        var minY = Math.Max(boundingBoxA.Top, boundingBoxB.Top);
+        var maxX = Math.Min(boundingBoxA.Right, boundingBoxB.Right);
+        var maxY = Math.Min(boundingBoxA.Bottom, boundingBoxB.Bottom);
+
+        var intersectionArea = Math.Max(maxY - minY, 0) * Math.Max(maxX - minX, 0);
+
+        return intersectionArea / (areaA + areaB - intersectionArea);
+    }
+
+    public IList<YoloBoundingBox> ParseOutputs(float[] yoloModelOutputs, float threshold = .3F)
+    {
+        var boxes = new List<YoloBoundingBox>();
+
+        for (int row = 0; row < ROW_COUNT; row++)
+        {
+            for (int column = 0; column < COL_COUNT; column++)
+            {
+                for (int box = 0; box < BOXES_PER_CELL; box++)
+                {
+                    var channel = (box * (CLASS_COUNT + BOX_INFO_FEATURE_COUNT));
+                    float confidence = GetConfidence(yoloModelOutputs, row, column, channel);
+
+                    if (confidence < threshold)
+                        continue;
+
+                    float[] predictedClasses = ExtractClasses(yoloModelOutputs, row, column, channel);
+                    var (topResultIndex, topResultScore) = GetTopResult(predictedClasses);
+                    var topScore = topResultScore * confidence;
+
+                    if (topScore < threshold)
+                        continue;
+
+                    BoundingBoxDimensions boundingBoxDimensions = ExtractBoundingBoxDimensions(yoloModelOutputs, row, column, channel);
+                    CellDimensions mappedBoundingBox = MapBoundingBoxToCell(row, column, box, boundingBoxDimensions);
+
+                    boxes.Add(new YoloBoundingBox()
+                    {
+                        Dimensions = new BoundingBoxDimensions
+                        {
+                            X = (mappedBoundingBox.X - mappedBoundingBox.Width / 2),
+                            Y = (mappedBoundingBox.Y - mappedBoundingBox.Height / 2),
+                            Width = mappedBoundingBox.Width,
+                            Height = mappedBoundingBox.Height,
+                        },
+                        Confidence = topScore,
+                        Label = labels[topResultIndex],
+                        BoxColor = classColors[topResultIndex]
+                    });
+                }
+            }
+        }
+
+        return boxes;
+    }
 }
